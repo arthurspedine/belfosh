01d0a8d836e880b6a8276a44833cc1a7c97bb3a8
Arthur Spedine <tucogamesytthegamesof@gmail.com>
2024-07-18 13:36:42 -0300

    fix: jwt secret

--- spring-bookshelf/src/main/java/br/com/spedine/bookshelf/infra/security/TokenService.java
+++ spring-bookshelf/src/main/java/br/com/spedine/bookshelf/infra/security/TokenService.java
@@ -16,7 +16,7 @@ import java.time.ZoneOffset;
 @Service
 public class TokenService {
 
-    @Value("{api.security.token.secret}")
+    @Value("${api.security.token.secret}")
     private String secret;
 
     public String genToken(User user) {
